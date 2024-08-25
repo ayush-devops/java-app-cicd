17254560c657dcf86bcc2da4ada8e91e039ff81b
root <root@ip-172-31-8-149.us-east-2.compute.internal>
2024-08-25 17:43:16 +0000

    ghjh

--- scripts/server_stop_clean.sh
+++ scripts/server_stop_clean.sh
@@ -4,6 +4,5 @@
 sudo service tomcat stop
 sudo mv /opt/tomcat/webapps/ROOT/hello-world-maven/index.html /opt/tomcat/webapps/ROOT/hello-world-maven/index-old.html
 sudo mv /opt/tomcat/webapps/ROOT/hello-world-maven/latest/index.html /opt/tomcat/webapps/ROOT/hello-world-maven/
-sudo rm -rf /opt/tomcat/webapps/ROOT/hello-world-maven/index-old.html /opt/tomcat/webapps/ROOT/hello-world-maven/latest/
 sudo rm -rf /opt/tomcat/webapps/ROOT/hello-world-maven/hello-world-maven.war
 
